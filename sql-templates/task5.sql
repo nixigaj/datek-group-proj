3e9460783ae82b1d180e0896cd51e4743122b0f9
Noviru <anton.nordstrom20021@gmail.com>
2024-12-09 10:32:48 +0100

    task 5 query 1 to 4

--- sql-templates/task5.sql
+++ sql-templates/task5.sql
@@ -1,11 +1,32 @@
 # Query 1:
-SELECT welcome_text
+SELECT description AS welcome_text
 FROM departments
 WHERE parent_title IS NULL
 LIMIT 1;
 
 # Query 2:
 
-SELECT title, root_url, description, logo, welcome_text
+SELECT title, root_url, description, logo
 FROM departments
 WHERE parent_title IS NULL;
+
+# Query 3:
+
+SELECT product_id, title, retail_price, description
+FROM products
+WHERE sale = TRUE
+ORDER BY retail_price ASC
+LIMIT 10;
+
+# Query 4:
+
+SELECT DISTINCT p.product_id, p.title, p.description, p.retail_price
+FROM products p
+JOIN has_keywords hk ON p.product_id = hk.product_id
+WHERE hk.keyword_id IN (
+    SELECT keyword_id
+    FROM has_keywords
+    WHERE product_id = 101
+) AND p.product_id != 101;
+
+
