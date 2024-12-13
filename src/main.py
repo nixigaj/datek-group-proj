ff5a793540229cb313009c500bfb7d5458ec7275
Felix Ljungkvist <felixljungkvist@hotmail.com>
2024-12-13 14:04:22 +0100

    separate into multiple files

--- src/main.py
+++ src/main.py
@@ -1,27 +1,19 @@
-from mysql import connector
-from mysql.connector import Error
-import pandas as pd
-import numpy as np
+#import pandas as pd
+from getpass import getpass
+from funcs import *
+from queries import *
 
-
-def create_server_connection(host_name, user_name, user_password, database_name):
-    connection = None
-    try:
-        connection = connector.connect(
-        host = host_name,
-        user = user_name,
-        passwd = user_password,
-        database = database_name,
-    )
-        print("MySQL database connection successful")
-    except Error as err:
-        print(f"Error: '{err}'")
-    return connection
-
+# ---------------------------------------------------- #
+# main code
+# ---------------------------------------------------- #
 
 host_name = "95.141.241.25"
 user_name = input("Enter username: ")
-user_password = input("Enter your password: ")
+user_password = getpass("Enter password: ")
 database = "datek"
 
-create_server_connection(host_name, user_name, user_password, database)
+connection = create_server_connection(host_name, user_name, user_password, database)
+
+#perform_database_query(connection, )
+
+
